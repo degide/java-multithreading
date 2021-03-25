6c56e3a31582bf38c0a19fbd984ad15ebdfd89d3
Harerimana Egide <egideharerimana085@gmail.com>
2021-03-25 21:26:23 +0200

    added new test file

--- src/com/multithreading/ExtendThradExample2.java
+++ src/com/multithreading/ExtendThradExample2.java
@@ -1,4 +1,34 @@
 package com.multithreading;
 
-public class ExtendThradExample2 {
+public class ExtendThradExample2 extends Thread{
+    private String thread_name;
+    private int num;
+
+    public ExtendThradExample2(String name, int num){
+        this.setThread_name(name);
+        this.setNum(num);
+    }
+
+    @Override
+    public void run() {
+        for(int i=0; i<this.num; i++){
+            System.out.println("From Thread "+this.getThread_name()+": "+i);
+        }
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getThread_name() {
+        return thread_name;
+    }
+
+    public void setThread_name(String thread_name) {
+        this.thread_name = thread_name;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
